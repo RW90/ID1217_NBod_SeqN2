a6d2eb6dd2c6da7db172a23ce963df63f19db3a7
RillMeister <riwa@kth.se>
2020-03-12 19:49:34 +0100

    First commit

---
+++ SeqN2.c
@@ -0,0 +1,15 @@
+#ifndef _REENTRANT 
+#define _REENTRANT 
+#endif 
+
+#include <pthread.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <time.h>
+#include <sys/time.h>
+
+int main(){
+
+    return 0;
+}
